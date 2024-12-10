891dd97320b0c0a000335da8f4dc77d21a0e1c58
Emily Jones <116977953+emilyyjx@users.noreply.github.com>
2024-12-09 23:40:49 -0500

    Update list.c

--- list.c
+++ list.c
@@ -11,130 +11,69 @@ Distribution: https://github.com/SasheO/operating-systems-project-3
 #include <string.h>
 #include "list.h"
 
-//insert node at the first location. setting head as NULL means this just creates and returns a new node
+// Create and insert a node at the first location
 struct node* createAndInsertU(struct node *head, int socket, char *username) {
-    
-   if(findU(head,username) == NULL) {
-           
-       //create a link
-       struct node *link = (struct node*) malloc(sizeof(struct node));
-
-       link->socket = socket;
-       strcpy(link->username,username);
-       
-       //point it to old first node
-       link->next = head;
-
-       //point first to new first node
-       head = link;
- 
-   }
-   else
-       printf("Duplicate: %s\n", username);
-   return head;
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    strcpy(new_node->username, username);
+    new_node->socket = socket;
+    new_node->next = head;
+    return new_node;
 }
 
-// insert already existing link to first location
-struct node* insertU(struct node *head, struct node *link) {        
-  //point it to old first node
-  if(findU(head,link->username) == NULL) {
-  link->next = head;
-
-  //point first to new first node
-  head = link;
-  }
-  else{
-    printf("Duplicate: %s\n", link->username);
-  }
-  return head;
+// Insert an already existing link to the first location
+struct node* insertU(struct node *nodelist, struct node *link) {
+    link->next = nodelist;
+    return link;
 }
 
-//find a link with given user
+// Find a node with a given username
 struct node* findU(struct node *head, char* username) {
-
-   //start from the first link
-   struct node* current = head;
-
-   //if list is empty
-   if(head == NULL) {
-      return NULL;
-   }
-
-   //navigate through list
-    while(strcmp(current->username, username) != 0) {
-	
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //go to next link
-         current = current->next;
-      }
-   }      
-	
-   //if username found, return the current Link
-   return current;
+    struct node *current = head;
+    while (current != NULL) {
+        if (strcmp(current->username, username) == 0) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
 }
 
-//remove link with given user. this only deals with the linked list itself and not with the list header so if the node is at the front of the list, it will have to be dealt with accordingly. returns null if user not there or error
-// returns node if node was removed or null if nothing removed
+// Remove a link with a given username
 struct node* removeU(struct node *head, char* username) {
+    struct node *current = head;
+    struct node *previous = NULL;
 
-   //start from the first link
-   struct node* current = head;
-   struct node* prev = NULL;
-
-   //if list is empty
-   if(head == NULL) {
-      return NULL;
-   }
-
-   //navigate through list
-    while(strcmp(current->username, username) != 0) {
-	
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //go to next link
-         prev = current;
-         current = current->next;
-      }
-   }      
-	
-   //if username found, remove and the current Link
-   if (strcmp(current->username, username)==0){
-    if (prev!=NULL){ // first link
-      prev->next = current->next;
-      current->next = NULL;
+    while (current != NULL) {
+        if (strcmp(current->username, username) == 0) {
+            if (previous == NULL) {
+                head = current->next; // Node is at the head
+            } else {
+                previous->next = current->next;
+            }
+            free(current);
+            return head;
+        }
+        previous = current;
+        current = current->next;
     }
-    else{
-      head = current->next;
+    return head;
+}
+
+// Rename a user
+void renameU(struct node *head, char* oldusername, char* newusername) {
+    struct node *user = findU(head, oldusername);
+    if (user != NULL) {
+        strcpy(user->username, newusername);
     }
-    free(current);
-    current=NULL;
-   }
-   return head;
-     
 }
 
-void renameU(struct node *head, char* oldusername, char* newusername){
-  struct node * link =  findU(head, oldusername);
-  if (link!=NULL){ 
-    strcpy(link->username, newusername);
-  }
+// Delete and deallocate every node in the list
+void deleteAllUs(struct node *head) {
+    struct node *current = head;
+    while (current != NULL) {
+        struct node *temp = current;
+        current = current->next;
+        free(temp);
+    }
 }
 
-void deleteAllUs(struct node *head){
-  struct node * curr = head;
-  struct node * next;
-
-  while (curr!=NULL){
-    next = curr->next;
-
-    free(curr);
-    curr = NULL;
-    
-    curr = next;
-  }
-}
